58cbe8eb4ac14b04902a0bcae5dfd1e5466a5ed1
Diogo Dias Beckman <diogodias2548@gmail.com>
2026-08-18 14:50:54 -0300

    Feat:foreach

---
+++ exercicio1/js/script.js
@@ -0,0 +1,30 @@
+const result = document.querySelector('#resultado');
+const formulario = [
+    {
+
+        Titulo: "Manutenção",
+        Descricao: "o ar esta pingando",
+        Responsável: "Pedro DIME",
+        DataConclusao: "10/05/2026"
+        
+    },
+    {
+        Titulo: "Atualizar senha ",
+        Descricao: "Minha senha expirou",
+        Responsável: "Joao DPA",
+        DataConclusao: "10/05/2026"
+    }
+    ];
+
+    
+
+    for(let i =0;i < formulario.length;i++){
+        const chaves = Object.keys(formulario[i])
+        for(let j = 0;j < chaves.length ; j++){
+            const chave = chaves[j];
+            result.innerHTML += `<li>${chave}: ${formulario[i][chave]}}</li>`   
+        }
+        
+        
+        }
+    
